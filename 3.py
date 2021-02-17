f8a0dc9bbaf3b3ace5adc31f62cfba8082ca126d
neahyo <horlaneyee@gmail.com>
2021-02-17 18:14:54 +0100

    Create 3.py
    Write a function that lists the files in a path with a specific file extension.

---
+++ 3.py
@@ -0,0 +1,19 @@
+#__________Write a function that lists the files in a path with a specific file extension.
+
+import os
+
+def list_files(path, ext):
+    '''
+    :path (str): path
+    :ext (str): file extension
+    '''
+    import os
+    
+    files = os.listdir(path)
+    files_ext = [i for i in files if i.endswith(ext)]
+    print("The files with", ext, "extension located in this path are:")
+    
+    for item in files_ext:
+        print("\t-", item)
+        
+ 
